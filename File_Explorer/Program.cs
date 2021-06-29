ab6747a86a89d0916ba5bfd72aba9fc715c70d09
vancoder1 <ivanzaporozhets25@gmail.com>
2021-06-29 19:35:42 +0300

    Create menu in console

---
+++ File_Explorer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Explorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menuItems = { "Item 1", "Item 2", "Item 3", "4", "5", "6"};
+            FileExplorer fe = new FileExplorer(menuItems);
+            FEConsoleMenu menu = new FEConsoleMenu(fe);
+
+            while(true)
+            {
+                if (menu.PrintMenu() == 1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
